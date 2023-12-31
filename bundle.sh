e0c62b50801f73fb0f36971f549d9802b979346e
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2024-01-01 00:49:01 +0200

    Update copyright for 2024

--- bundle.sh
+++ bundle.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#
+# @package   ExposeJoomla
+# @copyright Copyright (c)2020-2024 Nicholas K. Dionysopoulos
+# @license   GNU General Public License version 3, or later
+#
+
 pushd plugins/system/expose
 zip -r ../../../plg_system_expose.zip * .htaccess
 popd
